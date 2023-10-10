46f91fa47ea4ea05dfe3d5831265b33315e07c24
SarahNk1-Maker <sarahnaveed216@gmail.com>
2023-10-10 09:28:32 -0400

    refactor the database.js and editor.js

--- Develop/client/src/js/database.js
+++ Develop/client/src/js/database.js
@@ -1,40 +1,54 @@
 import { openDB } from 'idb';
 
-const initdb = async () =>
-  openDB('jate', 1, {
-    upgrade(db) {
-      if (db.objectStoreNames.contains('jate')) {
-        console.log('jate database already exists');
-        return;
-      }
-      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
-      console.log('jate database created');
-    },
-  });
+// Function to initialize the database
+const initDB = async () => {
+  try {
+    const db = await openDB('jate', 1, {
+      upgrade(db) {
+        if (!db.objectStoreNames.contains('jate')) {
+          db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
+          console.log('jate database created');
+        }
+      },
+    });
+    return db;
+  } catch (error) {
+    console.error('Error initializing the database:', error);
+    throw error;
+  }
+};
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => 
-console.log('Post to the ase');
-const jateDb = await openDB('jate', 1);
-const tx = jateDb.transaction('jate', 'readwrite');
-const store = tx.objectStore('jate');
-const request = store.add({ jate: content });
-const result = await request;
+// Function to add content to the database
+export const putDB = async (content) => {
+  try {
+    const jateDb = await initDB();
+    const tx = jateDb.transaction('jate', 'readwrite');
+    const store = tx.objectStore('jate');
+    const request = store.add({ jate: content });
+    await request;
+    console.log('Data added to the database successfully');
+  } catch (error) {
+    console.error('Error adding data to the database:', error);
+    throw error;
+  }
+};
 
-console.error('putDb not implemented',result);
+// Function to get all content from the database
+export const getDB = async () => {
+  try {
+    const jateDb = await initDB();
+    const tx = jateDb.transaction('jate', 'readonly');
+    const store = tx.objectStore('jate');
+    const request = store.getAll();
+    const result = await request;
+    console.log('Data retrieved from the database:', result);
+    return result;
+  } catch (error) {
+    console.error('Error retrieving data from the database:', error);
+    throw error;
+  }
+};
 
-// TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () =>{
-console.log('GET all from the database');
-const jateDb = await openDB('jate', 1);
-const tx = jateDb.transaction('jate', 'readonly');
-const store = tx.objectStore('jate');
-const request = store.getAll();
-const result = await request;
-console.log('result.value', result);
-return result;
-}
+// Initialize the database when the module is imported
+initDB();
 
-console.error('getDb not implemented');
-
-initdb();
